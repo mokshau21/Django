6c59b142d610ed0bea5e4de336c0b6620c474ee1
Moksha <mokshaupadhyaya@gmail.com>
2023-05-01 09:27:53 +0530

    This

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
 from first_app import views
 urlpatterns=[
-    re_path(r'^$',views.print,name='index')
+    re_path(r'^$',views.index,name='index')
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,5 +3,3 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Moksha")
-def print(request):
-    return HttpResponse("Upadhyaya")
